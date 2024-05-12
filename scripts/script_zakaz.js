d0655c4cf2af032d7491177c39bd661a1409b858
mastpsp <vr@vr>
2024-05-12 16:06:14 +0300

    add comments, change form and form-style. Add wdocx

--- scripts/script_zakaz.js
+++ scripts/script_zakaz.js
@@ -1,25 +1,27 @@
 // Получаем поле ввода номера телефона
-var phoneInput = document.getElementById('contactPhone');
+let phoneInput = document.getElementById('contactPhone');
 
 // Создаем маску для номера телефона
-var phoneMask = IMask(phoneInput, {
+let phoneMask = IMask(phoneInput, {
     mask: '+{7}(000)000-00-00'  // Задаем маску для номера телефона
 });
 
 
+// Функция для отображения/скрытия пароля
 function show_hide_password(target){
-	var input = document.getElementById('password-input');
+	let input = document.getElementById('password-input');
 	if (input.getAttribute('type') == 'password') {
-		target.classList.add('view');
-		input.setAttribute('type', 'text');
+		target.classList.add('view'); // Добавляем класс для изменения вида кнопки
+		input.setAttribute('type', 'text'); // Изменяем тип поля ввода на текст, чтобы отобразить пароль
 	} else {
-		target.classList.remove('view');
-		input.setAttribute('type', 'password');
+		target.classList.remove('view'); // Удаляем класс для изменения вида кнопки
+		input.setAttribute('type', 'password'); // Возвращаем тип поля ввода к паролю для скрытия его значения
 	}
-	return false;
+	return false; // Предотвращаем стандартное поведение ссылки
 }
 
 
+// Обработчик события отправки формы
 document.getElementById('purchaseForm').addEventListener('submit', function(event) {
-    alert('Форма успешно отправлена!');
+    alert('Форма успешно отправлена!'); // Выводим сообщение об успешной отправке формы
 });
