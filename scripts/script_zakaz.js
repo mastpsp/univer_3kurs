cf5c18015d68dda017ab0618ab0ae34858def4a0
mastpsp <vr@vr>
2024-05-12 14:52:17 +0300

    relocate scripts

--- scripts/script_zakaz.js
+++ scripts/script_zakaz.js
@@ -1,5 +1,25 @@
-document.getElementById('purchaseForm').addEventListener('submit', function(event) {
-    // event.preventDefault();
+// Получаем поле ввода номера телефона
+var phoneInput = document.getElementById('contactPhone');
 
+// Создаем маску для номера телефона
+var phoneMask = IMask(phoneInput, {
+    mask: '+{7}(000)000-00-00'  // Задаем маску для номера телефона
+});
+
+
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
+
+
+document.getElementById('purchaseForm').addEventListener('submit', function(event) {
     alert('Форма успешно отправлена!');
 });
